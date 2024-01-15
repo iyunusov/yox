c389eeb6220a404ac51116de34b06ed15f726e7f
Islomjon Yunusov <islomjon-yunusov@mail.ru>
2024-01-16 01:08:38 +0500

    force-dynamic added to langing page

--- src/app/(listing)/page.tsx
+++ src/app/(listing)/page.tsx
@@ -1,6 +1,7 @@
 import Landing from "@/components/Landing/Landing";
 import { Product } from "@/types/product";
 import { fetchAllProducts } from "@/utils/fetchers";
+export const dynamic = 'force-dynamic';
 
 export default async function Page() {
   let products: Product[] = await fetchAllProducts()
