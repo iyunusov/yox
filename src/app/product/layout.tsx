df8ecc0bff1aaf7f1f2dea726b50325c8c298923
Islomjon Yunusov <islomjon-yunusov@mail.ru>
2024-01-16 00:05:51 +0500

    ts type fixed

--- src/app/product/layout.tsx
+++ src/app/product/layout.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 
-export default function Layout({ children }: { children: ChildNode }) {
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
       {children}
