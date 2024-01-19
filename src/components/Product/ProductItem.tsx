0e3881f96c0327dae1a8174ea0e2481e82b8967b
Islomjon Yunusov <islomjon-yunusov@mail.ru>
2024-01-19 16:07:36 +0500

    CardImage chagned to next/image for better performance.

--- src/components/Product/ProductItem.tsx
+++ src/components/Product/ProductItem.tsx
@@ -1,8 +1,9 @@
 'use client'
-import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography } from "@mui/material";
+import { Card, CardActionArea, CardContent, CardMedia, Grid, Typography, useTheme } from "@mui/material";
 import { useRouter } from "next/navigation";
 import { Product } from "@/types/product";
 import ProductItemDelete from "./ProductItemDelete";
+import Image from "next/image";
 
 interface ProductItemProps {
   product: Product;
@@ -11,6 +12,7 @@ interface ProductItemProps {
 export default function ProductItem ({ product }: ProductItemProps) {
   const { id, title, description, image} = product;
   const router = useRouter();
+  const theme = useTheme();
 
   const onProductClick = () => {
     router.push(`/product/${id}`)
@@ -29,12 +31,19 @@ export default function ProductItem ({ product }: ProductItemProps) {
       <CardActionArea>
         <Card sx={{ maxWidth: 345 }}>
           <ProductItemDelete productId={id} imageUrl={image} />
-          <CardMedia
-            component="img"
-            alt="preview Image"
-            height="140"
-            image={image}
-          />
+          <div style={{ position: 'relative', width: 'inherit', height: '140px' }}>
+            <Image
+              src={image}
+              style={{ objectFit: "cover" }}
+              alt="preview Image"
+              fill
+              sizes={`
+                (max-width: ${theme.breakpoints.values.xs}px) 100vw,
+                (max-width: ${theme.breakpoints.values.md}px) 345w
+              `}
+              priority={true}
+              />
+          </div>
           <CardContent>
             <Typography
               gutterBottom
--- src/components/Product/ProductList.tsx
+++ src/components/Product/ProductList.tsx
@@ -10,7 +10,7 @@ export default function ProductList({ products: productsProp }: ProductListProps
   const products = useFilteredProducts(productsProp);
 
   return (
-    <Grid container spacing={{ xs: 0, sm: 2, md: 4 }} position={'relative'}>
+    <Grid container spacing={{ xs: 4, sm: 2, md: 4, }} position={'relative'}>
       {products?.map((product) => {
         return (
           <ProductItem key={product.id} product={product} />
